a030c881d8100e38f518e41d2297117cf64600b1
seaton <wangxd1208@163.com>
2017-04-27 10:12:18 +0800

    modify

--- static/20170425/js/common.js
+++ static/20170425/js/common.js
@@ -3,6 +3,34 @@
  */
 $(function () {
 
+    /* 设置高度 */
+    var _height = $(".no-move").height();
+    $(".placeholder").height(_height);
+
+    function setPop (type) {
+        var _html = "";
+        if (type == 1) {
+            _html = "<div class='pop-body'>" +
+                "<img class='width-all block' src='./images/pop-top1.png' />" +
+                "<div class='bg-1 desc-1 '>" +
+                "<strong class='block text-center font-1 cor-2'>下载顾问工具包，查看全部精彩聊天内容！</strong>" +
+                "<p class='desc-2 text-center font-0 cor-1'>亲爱的，下载顾问工具包，使用美容顾问账号登录，更快更省流量的同时，即刻参与直播互动噢！</p>" +
+                "<div class='foot text-center'>" +
+                "<span class='no-down font-4 cor-8 J-no-download'>暂不下载</span><span class='font-4 cor-7 J-go-download'>立即前往下载</span>" +
+                "</div></div></div>";
+        } else {
+            _html = "<div class='pop-body'>" +
+                "<img class='width-all block' src='./images/pop-top2.png' />" +
+                "<div class='bg-1 desc-1 '>" +
+                "<strong class='block text-center font-1 cor-2'>下载顾问工具包，即刻参与聊天活动！</strong>" +
+                "<p class='desc-2 text-center font-0 cor-1'>亲爱的，下载顾问工具包，使用美容顾问账号登录，更快更省流量的同时，即刻参与直播互动噢！</p>" +
+                "<div class='foot text-center'>" +
+                "<span class='no-down font-4 cor-8 J-no-download'>暂不下载</span><span class='font-4 cor-7 J-go-download'>立即前往下载</span>" +
+                "</div></div></div>";
+        }
+        $(".download-pop").html(_html).show();
+    }
+
     $(".J-switch-tab").click(function () { // tab 切换
         var _tab = $(this).attr("data-tab");
         var _line = $(".J-line");
@@ -27,16 +55,16 @@ $(function () {
     });
 
     $(".J-download").click(function () { // 点击下载，激活下载窗口
-        $(".download-pop").show();
+        setPop(1);
     });
 
-    $(".J-no-download").click(function () { // 点击暂不下载
-        $(".download-pop").hide();
+    $("body").on("click", ".J-no-download", function () { // 点击暂不下载
+        $(".download-pop").empty().hide();
     });
 
-    $(".J-go-download").click(function () { // 点击立即下载
+    $("body").on("click", ".J-go-download", function () { // 点击立即下载
         console.log("click the download");
-        $(".download-pop").hide();
+        $(".download-pop").empty().hide();
     });
 
     $(".J-close").click(function () { // 关闭下载区域
